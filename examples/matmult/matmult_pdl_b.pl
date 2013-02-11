3732de05bef1672db3c286c219adfc845c7a48b4
marioeroy <marioeroy@280a465b-a97f-b54b-bb6b-c9981b522c87>
2013-02-11 11:40:43 +0000

    Added 5 matrix multiplication examples to be included with MCE 1.4

---
+++ examples/matmult/matmult_pdl_b.pl
@@ -0,0 +1,56 @@
+#!/usr/bin/env perl
+
+##
+## Usage:
+##    perl matmult_pdl_b.pl   1024  ## Default size is 512:  $c = $a x $b
+##
+
+use strict;
+use warnings;
+
+use FindBin;
+use lib "$FindBin::Bin/../../lib";
+
+my $prog_name = $0; $prog_name =~ s{^.*[\\/]}{}g;
+
+use Time::HiRes qw(time);
+
+use PDL;
+
+###############################################################################
+ # * # * # * # * # * # * # * # * # * # * # * # * # * # * # * # * # * # * # * #
+###############################################################################
+
+my $tam = shift;
+   $tam = 512 unless (defined $tam);
+
+unless ($tam > 1) {
+   print STDERR "Error: $tam is not 2 or greater. Exiting.\n";
+   exit 1;
+}
+
+my $a = sequence $tam,$tam;
+my $b = sequence $tam,$tam;
+my $c;
+
+my $max_parallel_level = 1;              ## Levels deep to parallelize
+my @p = ( );                             ## For MCE results - must be global
+
+my $start = time();
+
+$c = $a x $b;                            ## Start matrix multiplication
+
+my $end = time();
+
+printf STDERR "\n## $prog_name $tam: compute time: %0.03f secs\n\n",
+   $end - $start;
+
+my $dim_1 = $tam - 1;
+
+print "## (0,0) ", $c->at(0,0), "  ($dim_1,$dim_1) ", $c->at($dim_1,$dim_1);
+print "\n\n";
+
+###############################################################################
+ # * # * # * # * # * # * # * # * # * # * # * # * # * # * # * # * # * # * # * #
+###############################################################################
+
