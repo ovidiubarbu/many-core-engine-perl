18ff187ebb58473dbecb7d2d4a79b63a1ab51a3d
marioeroy <marioeroy@280a465b-a97f-b54b-bb6b-c9981b522c87>
2013-02-11 01:00:13 +0000

    Added opening/closing parens for _croak statements

--- lib/MCE.pm
+++ lib/MCE.pm
@@ -1303,33 +1303,33 @@ sub _validate_args {
    my $_tag = 'MCE::_validate_args';
 
    if (defined $_s->{input_data} && ref $_s->{input_data} eq '') {
-      _croak "$_tag: '$_s->{input_data}' does not exist"
+      _croak("$_tag: '$_s->{input_data}' does not exist")
          unless (-e $_s->{input_data});
    }
 
-   _croak "$_tag: 'use_slurpio' is not 0 or 1"
+   _croak("$_tag: 'use_slurpio' is not 0 or 1")
       if ($_s->{use_slurpio} && $_s->{use_slurpio} !~ /\A[01]\z/);
-   _croak "$_tag: 'job_delay' is not valid"
+   _croak("$_tag: 'job_delay' is not valid")
       if ($_s->{job_delay} && $_s->{job_delay} !~ /\A[\d\.]+\z/);
-   _croak "$_tag: 'spawn_delay' is not valid"
+   _croak("$_tag: 'spawn_delay' is not valid")
       if ($_s->{spawn_delay} && $_s->{spawn_delay} !~ /\A[\d\.]+\z/);
-   _croak "$_tag: 'submit_delay' is not valid"
+   _croak("$_tag: 'submit_delay' is not valid")
       if ($_s->{submit_delay} && $_s->{submit_delay} !~ /\A[\d\.]+\z/);
 
-   _croak "$_tag: 'on_post_exit' is not a CODE reference"
+   _croak("$_tag: 'on_post_exit' is not a CODE reference")
       if ($_s->{on_post_exit} && ref $_s->{on_post_exit} ne 'CODE');
-   _croak "$_tag: 'on_post_run' is not a CODE reference"
+   _croak("$_tag: 'on_post_run' is not a CODE reference")
       if ($_s->{on_post_run} && ref $_s->{on_post_run} ne 'CODE');
-   _croak "$_tag: 'user_error' is not a CODE reference"
+   _croak("$_tag: 'user_error' is not a CODE reference")
       if ($_s->{user_error} && ref $_s->{user_error} ne 'CODE');
-   _croak "$_tag: 'user_output' is not a CODE reference"
+   _croak("$_tag: 'user_output' is not a CODE reference")
       if ($_s->{user_output} && ref $_s->{user_output} ne 'CODE');
 
-   _croak "$_tag: 'flush_file' is not 0 or 1"
+   _croak("$_tag: 'flush_file' is not 0 or 1")
       if ($_s->{flush_file} && $_s->{flush_file} !~ /\A[01]\z/);
-   _croak "$_tag: 'flush_stderr' is not 0 or 1"
+   _croak("$_tag: 'flush_stderr' is not 0 or 1")
       if ($_s->{flush_stderr} && $_s->{flush_stderr} !~ /\A[01]\z/);
-   _croak "$_tag: 'flush_stdout' is not 0 or 1"
+   _croak("$_tag: 'flush_stdout' is not 0 or 1")
       if ($_s->{flush_stdout} && $_s->{flush_stdout} !~ /\A[01]\z/);
 
    $_s->_validate_args_s();
@@ -1338,7 +1338,7 @@ sub _validate_args {
       for my $_t (@{ $_s->{user_tasks} }) {
          $_s->_validate_args_s($_t);
 
-         _croak "$_tag: 'task_end' is not a CODE reference"
+         _croak("$_tag: 'task_end' is not a CODE reference")
             if ($_t->{task_end} && ref $_t->{task_end} ne 'CODE');
       }
    }
@@ -1357,38 +1357,38 @@ sub _validate_args_s {
 
    my $_tag = 'MCE::_validate_args_s';
 
-   _croak "$_tag: 'chunk_size' is not valid"
+   _croak("$_tag: 'chunk_size' is not valid")
       if (defined $_s->{chunk_size} && (
          $_s->{chunk_size} !~ /\A\d+\z/ or $_s->{chunk_size} == 0
       ));
-   _croak "$_tag: 'max_workers' is not valid"
+   _croak("$_tag: 'max_workers' is not valid")
       if (defined $_s->{max_workers} && (
          $_s->{max_workers} !~ /\A\d+\z/ or $_s->{max_workers} == 0
       ));
 
-   _croak "$_tag: 'use_threads' is not 0 or 1"
+   _croak("$_tag: 'use_threads' is not 0 or 1")
       if ($_s->{use_threads} && $_s->{use_threads} !~ /\A[01]\z/);
-   _croak "$_tag: 'user_begin' is not a CODE reference"
+   _croak("$_tag: 'user_begin' is not a CODE reference")
       if ($_s->{user_begin} && ref $_s->{user_begin} ne 'CODE');
-   _croak "$_tag: 'user_func' is not a CODE reference"
+   _croak("$_tag: 'user_func' is not a CODE reference")
       if ($_s->{user_func} && ref $_s->{user_func} ne 'CODE');
-   _croak "$_tag: 'user_end' is not a CODE reference"
+   _croak("$_tag: 'user_end' is not a CODE reference")
       if ($_s->{user_end} && ref $_s->{user_end} ne 'CODE');
 
    if (defined $_s->{sequence}) {
       my $_seq = $_s->{sequence};
 
-      _croak "$_tag: cannot specify both 'input_data' and 'sequence'"
+      _croak("$_tag: cannot specify both 'input_data' and 'sequence'")
          if (defined $self->{input_data});
-      _croak "$_tag: 'sequence' is not a HASH reference"
+      _croak("$_tag: 'sequence' is not a HASH reference")
          if (ref $_seq ne 'HASH');
-      _croak "$_tag: 'begin' is not defined for sequence"
+      _croak("$_tag: 'begin' is not defined for sequence")
          unless (defined $_seq->{begin});
-      _croak "$_tag: 'end' is not defined for sequence"
+      _croak("$_tag: 'end' is not defined for sequence")
          unless (defined $_seq->{end});
 
       for (qw(begin end step)) {
-         _croak "$_tag: '$_' is not valid for sequence"
+         _croak("$_tag: '$_' is not valid for sequence")
             if (defined $_seq->{$_} && (
                $_seq->{$_} eq '' || $_seq->{$_} !~ /\A-?\d*\.?\d*\z/
             ));
@@ -1402,7 +1402,7 @@ sub _validate_args_s {
            ($_seq->{step} > 0 && $_seq->{begin} > $_seq->{end}) ||
            ($_seq->{step} == 0)
       ) {
-         _croak "$_tag: impossible 'step' size for sequence";
+         _croak("$_tag: impossible 'step' size for sequence");
       }
    }
 
@@ -2695,7 +2695,7 @@ sub _dispatch_child {
 
    my $_pid = fork();
 
-   _croak "MCE::_dispatch_child: Failed to spawn worker $_wid: $!"
+   _croak("MCE::_dispatch_child: Failed to spawn worker $_wid: $!")
       unless (defined $_pid);
 
    unless ($_pid) {
@@ -2743,7 +2743,7 @@ sub _dispatch_thread {
       $self, $_wid, $_task, $_task_id, $_task_wid, $_params
    );
 
-   _croak "MCE::_dispatch_thread: Failed to spawn worker $_wid: $!"
+   _croak("MCE::_dispatch_thread: Failed to spawn worker $_wid: $!")
       unless (defined $_thr);
 
    if (defined $_thr) {
