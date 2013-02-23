8820f5ac9a0c835f427f89b9957fb6469789840e
marioeroy <marioeroy@280a465b-a97f-b54b-bb6b-c9981b522c87>
2013-02-23 00:50:10 +0000

    Additional updates to strassen examples. Added strassen_pdl_p.pl combining 2 levels -- 49 workers.

--- examples/matmult/strassen_pdl_n.pl
+++ examples/matmult/strassen_pdl_n.pl
@@ -138,12 +138,17 @@ sub strassen {
    sum_m($b11, $b22, $a12, $nTam);               ## Reuse $a12 as $t2
    $mce->send([ $t1, $a12, 1, $nTam ]);          ## Reuse $a12 as $t2
 
+   undef $a11;             undef $a21; undef $a22;
+   undef $b11; undef $b12; undef $b21; undef $b22;
+
    $mce->run();
 
    $p2 = $p[2]; $p3 = $p[3]; $p4 = $p[4]; $p5 = $p[5];
    $p1 = $p[1]; $p6 = $p[6]; $p7 = $p[7];
 
    calc_m($p1, $p2, $p3, $p4, $p5, $p6, $p7, $c, $nTam, $t1, $a12);
+
+   @p = ();
 
    return;
 }
