285ee5a31eb8143e7beef6c19436f977dbfe167d
marioeroy <marioeroy@280a465b-a97f-b54b-bb6b-c9981b522c87>
2013-03-15 00:42:05 +0000

    MCE parity across the board -- the most stable ever, from Cygwin to Windows and obviously UNIX.

--- trunk/lib/MCE/Signal.pm
+++ trunk/lib/MCE/Signal.pm
@@ -24,7 +24,7 @@ BEGIN {
    $prog_name    =~ s{^.*[\\/]}{}g;
 }
 
-our $VERSION = '1.406';
+our $VERSION = '1.407';
 $VERSION = eval $VERSION;
 
 our $tmp_dir = undef;
@@ -430,7 +430,7 @@ MCE::Signal - Provides tmp_dir creation & signal handling for Many-Core Engine.
 
 =head1 VERSION
 
-This document describes MCE::Signal version 1.406
+This document describes MCE::Signal version 1.407
 
 =head1 SYNOPSIS
 
