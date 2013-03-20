100174deaa38db7661d63e68555b67658ad00733
marioeroy <marioeroy@280a465b-a97f-b54b-bb6b-c9981b522c87>
2013-03-20 01:47:11 +0000

    Update to MCE::Signal, added new options: -no_kill9, -no_sigmsg. Minor tweaks.

--- trunk/lib/MCE/Signal.pm
+++ trunk/lib/MCE/Signal.pm
@@ -24,7 +24,7 @@ BEGIN {
    $prog_name    =~ s{^.*[\\/]}{}g;
 }
 
-our $VERSION = '1.407';
+our $VERSION = '1.408';
 $VERSION = eval $VERSION;
 
 our $tmp_dir = undef;
@@ -42,10 +42,12 @@ our %EXPORT_TAGS = (
 ###############################################################################
 
 sub _croak { require Carp; goto &Carp::croak; }
-sub _usage { _croak 'usage: [-use_dev_shm] [-keep_tmp_dir]'; }
+sub _usage { _croak "MCE::Signal error: $_[0] is not a valid option"; }
 sub _flag  { 1; }
 
 my $_keep_tmp_dir = 0;
+my $_no_kill9     = 0;
+my $_no_sigmsg    = 0;
 my $_loaded;
 
 sub import {
@@ -59,11 +61,16 @@ sub import {
    my $_use_dev_shm = 0;
 
    while (my $_arg = shift) {
+
       $_keep_tmp_dir = _flag() and next if ($_arg eq '-keep_tmp_dir');
+      $_no_kill9     = _flag() and next if ($_arg eq '-no_kill9');
+      $_no_sigmsg    = _flag() and next if ($_arg eq '-no_sigmsg');
       $_no_setpgrp   = _flag() and next if ($_arg eq '-no_setpgrp');
       $_setpgrp      = _flag() and next if ($_arg eq '-setpgrp');
       $_use_dev_shm  = _flag() and next if ($_arg eq '-use_dev_shm');
-      _usage() if ($_arg =~ /^-/);
+
+      _usage($_arg) if ($_arg =~ /^-/);
+
       push @_export_args, $_arg;
    }
 
@@ -110,8 +117,7 @@ sub import {
 ##
 ###############################################################################
 
-our $mce_sess_dir_ref = undef;
-our $mce_spawned_ref  = undef;
+our ($mce_sess_dir_ref, $mce_spawned_ref);
 
 ## Set traps to catch signals.
 $SIG{XCPU} = \&stop_and_exit if (exists $SIG{XCPU});   ## UNIX SIG 24
@@ -244,7 +250,9 @@ sub sys_cmd {
                }
 
                ## Display error message.
-               print STDERR "\n## $prog_name: $_err_msg\n" if ($_err_msg);
+               print STDERR "\n## $prog_name: $_err_msg\n"
+                  if ($_err_msg && $_no_sigmsg == 0);
+
                open my $_FH, "> $tmp_dir/killed"; close $_FH;
 
                ## Signal process group to terminate.
@@ -252,7 +260,7 @@ sub sys_cmd {
 
                ## Pause a bit.
                if ($_sig_name ne 'PIPE') {
-                  select(undef, undef, undef, 0.066) for (1..4);
+                  select(undef, undef, undef, 0.066) for (1..3);
                }
             }
 
@@ -280,8 +288,11 @@ sub sys_cmd {
 
             ## Signal process group to die.
             if ($_is_sig == 1) {
-               print STDERR "\n" if ($_sig_name ne 'PIPE');
-               kill('KILL', -$$, $main_proc_id);
+               print STDERR "\n"
+                  if ($_sig_name ne 'PIPE' && $_no_sigmsg == 0);
+
+               kill('KILL', -$$, $main_proc_id)
+                  if ($_sig_name eq 'PIPE' || $_no_kill9 == 0);
             }
          }
       }
@@ -289,7 +300,7 @@ sub sys_cmd {
       ## ----------------------------------------------------------------------
 
       ## For child processes.
-      if ($$ != $main_proc_id && $_is_sig == 1) {
+      if ($$ != $main_proc_id && $_is_sig == 1 && -d $tmp_dir) {
 
          ## Obtain lock.
          open my $CHILD_LOCK, '+>>', "$tmp_dir/child.lock";
@@ -301,7 +312,7 @@ sub sys_cmd {
          if ($_handler_cnt == 1) {
 
             ## Notify the main process that I've died.
-            if ($_sig_name eq '__DIE__') {
+            if ($_sig_name eq '__DIE__' && ! -f "$tmp_dir/died") {
                open my $_FH, "> $tmp_dir/died"; close $_FH;
             }
 
@@ -320,8 +331,8 @@ sub sys_cmd {
       ## ----------------------------------------------------------------------
 
       ## Exit thread/process with status.
-      if ($_is_sig == 1) {
-         select(undef, undef, undef, 0.066) for (1..8);
+      if ($_is_sig == 1 && $_no_kill9 == 0) {
+         select(undef, undef, undef, 0.066) for (1..6);
       }
 
       threads->exit($_exit_status) if ($has_threads && threads->can('exit'));
@@ -430,11 +441,11 @@ MCE::Signal - Provides tmp_dir creation & signal handling for Many-Core Engine.
 
 =head1 VERSION
 
-This document describes MCE::Signal version 1.407
+This document describes MCE::Signal version 1.408
 
 =head1 SYNOPSIS
 
- use MCE::Signal qw( [-keep_tmp_dir] [-no_setpgrp] [-use_dev_shm] );
+ use MCE::Signal qw( [-keep_tmp_dir] [-use_dev_shm] );
 
 =head1 DESCRIPTION
 
@@ -458,9 +469,28 @@ just pass the -setpgrp option to MCE::Signal.
  use MCE::Signal qw(-setpgrp);
  use MCE;
 
-Nothing is exported by default. Exportable are 1 variable and 2 subroutines:
+The following are available arguments and their meanings.
 
- $tmp_dir          - Path to temporary directory.
+ -keep_tmp_dir     - The temporary directory is not removed during exiting
+                     A message is displayed with the location afterwards
+
+ -use_dev_shm      - Create the temporary directory under /dev/shm
+
+ -no_kill9         - Do not kill -9 after receiving a signal to terminate
+ -no_sigmsg        - Do not display a message when receiving a signal
+
+ -setpgrp          - Calls setpgrp to set the process group for the process
+
+                     Specify this option to ensure all workers terminate
+                     when reading STDIN like so:
+                        cat big_input_file | ./mce_script.pl | head -10
+
+                     This works fine without the -setpgrp option:
+                        ./mce_script.pl < big_input_file | head -10
+
+Nothing is exported by default. Exportable are 1 variable and 2 subroutines.
+
+ $tmp_dir          - Path to the temporary directory.
  stop_and_exit     - Described below
  sys_cmd           - Described below
 
