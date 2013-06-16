122a0d7745736ef871f18ff13765b7c16270320d
marioeroy <marioeroy@280a465b-a97f-b54b-bb6b-c9981b522c87>
2013-06-16 16:19:33 +0000

    Create a 1.414 dir

---
+++ tags/MCE-1.414/examples/matmult/matmult_base.pl
@@ -0,0 +1,49 @@
+#!/usr/bin/env perl
+
+##
+## Usage:
+##    perl matmult_base.pl 1024                    ## Default matrix size 512
+##
+
+use strict;
+use warnings;
+
+use Cwd qw(abs_path);
+use lib abs_path . "/../../lib";
+
+my $prog_name = $0; $prog_name =~ s{^.*[\\/]}{}g;
+
+use Time::HiRes qw(time);
+
+use PDL;
+
+###############################################################################
+ # * # * # * # * # * # * # * # * # * # * # * # * # * # * # * # * # * # * # * #
+###############################################################################
+
+my $tam = @ARGV ? shift : 512;
+
+if ($tam !~ /^\d+$/ || $tam < 2) {
+   die "error: $tam must be an integer greater than 1.\n";
+}
+
+my $cols = $tam;
+my $rows = $tam;
+
+my $a = sequence $cols,$rows;
+my $b = sequence $rows,$cols;
+
+my $start = time();
+my $c = $a x $b;                         ## Performs matrix multiplication
+my $end = time();
+
+## Print out the results -- use same pairs to match David Mertens' output.
+printf "\n## $prog_name $tam: compute time: %0.03f secs\n\n", $end - $start;
+
+for my $pair ([0, 0], [324, 5], [42, 172], [$rows-1, $rows-1]) {
+   my ($col, $row) = @$pair; $col %= $rows; $row %= $rows;
+   printf "## (%d, %d): %s\n", $col, $row, $c->at($col, $row);
+}
+
+print "\n";
+
