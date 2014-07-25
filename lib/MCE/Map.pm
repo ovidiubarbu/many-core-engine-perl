0a689f09ae8d24035e52e29adb0bda1596731b32
marioeroy <marioeroy@280a465b-a97f-b54b-bb6b-c9981b522c87>
2014-07-25 04:05:01 +0000

    Release 1.515

--- lib/MCE/Map.pm
+++ lib/MCE/Map.pm
@@ -14,7 +14,7 @@ use Scalar::Util qw( looks_like_number );
 use MCE;
 use MCE::Util;
 
-our $VERSION = '1.514'; $VERSION = eval $VERSION;
+our $VERSION = '1.515'; $VERSION = eval $VERSION;
 
 ###############################################################################
 ## ----------------------------------------------------------------------------
@@ -398,7 +398,7 @@ MCE::Map - Parallel map model similar to the native map function
 
 =head1 VERSION
 
-This document describes MCE::Map version 1.514
+This document describes MCE::Map version 1.515
 
 =head1 SYNOPSIS
 
