36b4b7da749f6dfa61a96e2e2dd4d730cb139e94
marioeroy <marioeroy@280a465b-a97f-b54b-bb6b-c9981b522c87>
2014-10-27 21:22:19 +0000

    Allow for multiple workers for the 1st task.

--- trunk/examples/files_flow.pl
+++ trunk/examples/files_flow.pl
@@ -4,14 +4,18 @@ use strict; use warnings;
 
 use Cwd qw(abs_path);
 use lib abs_path . "/../lib";
+use Time::HiRes "sleep";
 
-## Same logic as files_mce.pl; via the MCE::Flow model.
+## Same logic as in files_mce.pl, but with the MCE::Flow model.
 ## usage: ./files_flow.pl [ startdir [0|1] ]
 
 use MCE::Flow;
 use MCE::Queue;
 
+my $D = MCE::Queue->new(queue => [ $ARGV[0] || '.' ]);
 my $F = MCE::Queue->new(fast => defined $ARGV[1] ? $ARGV[1] : 1);
+
+my $providers = 3;
 my $consumers = 8;
 
 MCE::Flow::init {
@@ -23,14 +27,17 @@ MCE::Flow::init {
    }
 };
 
+## Override any MCE options and run. Notice how max_workers and
+## task_name take an anonymous array to configure both tasks.
+
 mce_flow {
-   ## MCE options may be specified here or MCE::Flow::init above
-   max_workers => [ 1, $consumers ], task_name => [ 'dir', 'file' ],
-   user_args => [ $ARGV[0] || '.' ]
+   max_workers => [ $providers, $consumers ],
+   task_name   => [ 'dir', 'file' ]
 },
 sub {
-   ## User task: dir
-   my $D = MCE::Queue->new(queue => [ MCE->user_args->[0] ]);
+   ## Dir Task. Allow time for wid 1 to enqueue any dir entries.
+   ## Otherwise, workers (wid 2+) may terminate early.
+   sleep 0.1 if MCE->task_wid > 1;
 
    while (defined (my $dir = $D->dequeue_nb)) {
       my (@files, @dirs); foreach (glob("$dir/*")) {
@@ -42,11 +49,14 @@ sub {
    }
 },
 sub {
-   ## User task: file
+   ## File Task.
    while (defined (my $file = $F->dequeue)) {
       MCE->say($file);
    }
 };
+
+## Workers persist in models. This may be ommitted. It will run
+## automatically during exiting if not already called.
 
 MCE::Flow::finish;
 
