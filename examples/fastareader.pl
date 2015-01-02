988b91dda88b20826f321e395bcdb4fcb5161ad1
marioeroy <marioeroy@280a465b-a97f-b54b-bb6b-c9981b522c87>
2015-01-02 19:49:37 +0000

    Updated the fastareader.pl example.

--- examples/fastareader.pl
+++ examples/fastareader.pl
@@ -3,23 +3,24 @@
 use strict;
 use warnings;
 
+## usage: fastareader.pl [ /path/to/fastafile.fa ]
+
 package BioUtil::Seq;
 
 ## The original plan was to run CPAN BioUtil::Seq::FastaReader in parallel.
 ## I thought faster was possible if logic processed records versus lines.
 ## https://gist.github.com/marioroy/4b85483b16a950255b8d
-## Usage: fastareader.pl [ /path/to/fastafile.fa ]
 ##
 ## Two million clusters taken from uniref100.fasta.gz (2013_12).
 ## gunzip -c uniref100.fasta.gz | head -15687827 > uniref.fasta
 ##
 ## Serial Code (without MCE)
-##   Line driven:   17.027s       CPAN FastaReader, BioUtil-2014.1226
-##   Record driven:  9.123s       Nearly 2x faster serially
-## 
+##   Line driven:   15.649s       CPAN FastaReader, BioUtil-2014.1226
+##   Record driven:  8.057s       Nearly 2x faster serially
+##
 ## MCE Code -- Parallelization
-##   Line driven:    4.992s       CPAN FastaReader, BioUtil-2014.1226
-##   Record driven:  2.767s
+##   Line driven:    4.872s       CPAN FastaReader, BioUtil-2014.1226
+##   Record driven:  2.507s
 
 sub FastaReader {
    my ($file, $not_trim) = @_;
@@ -35,7 +36,6 @@ sub FastaReader {
 
    return sub {
       return if $finished;
-
       local $/ = "\n>";                     ## set input record separator
 
       while (<$fh>) {
@@ -45,18 +45,16 @@ sub FastaReader {
          chop if substr($_, -1, 1) eq '>';  ## trim trailing ">"
 
          $pos = index($_, "\n");            ## extract header and sequence data
-         $hdr = substr($_, 0, $pos);
+         $hdr = substr($_, 0, $pos + 1);
          $seq = substr($_, $pos + 1);
 
-         chop $hdr if substr($hdr, -1, 1) eq "\r";  ## trim trailing "\r"
-
-         if ($not_trim) {
-            $seq =~ s/^\s*\r?\n//mg;        ## trim blank lines only
-         } else {
-            $seq =~ tr/ \t\r\n//d;          ## trim white space
+         unless ($not_trim) {
+            chop $hdr;                                 ## trim trailing "\n"
+            chop $hdr if substr($hdr, -1, 1) eq "\r";  ## trim trailing "\r"
+            $seq =~ tr/ \t\r\n//d;                     ## trim white space
          }
 
-         return [ $hdr, $seq ] if length $hdr;
+         return [ $hdr, $seq ];
       }
 
       close $fh unless $is_stdin;
