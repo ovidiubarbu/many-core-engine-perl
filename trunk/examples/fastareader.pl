9d90b49cb5d0207c24690ae449651b4dbfb4e486
marioeroy <marioeroy@280a465b-a97f-b54b-bb6b-c9981b522c87>
2015-01-03 07:54:09 +0000

    Updated the fastareader.pl example.

--- trunk/examples/fastareader.pl
+++ trunk/examples/fastareader.pl
@@ -5,22 +5,24 @@ use warnings;
 
 ## usage: fastareader.pl [ /path/to/fastafile.fa ]
 
+###############################################################################
+
 package BioUtil::Seq;
 
 ## The original plan was to run CPAN BioUtil::Seq::FastaReader in parallel.
 ## I thought faster was possible if logic processed records versus lines.
-## https://gist.github.com/marioroy/4b85483b16a950255b8d
+## https://gist.github.com/marioroy/1294672e8e3ba42cb684 (for most current)
 ##
 ## Two million clusters taken from uniref100.fasta.gz (2013_12).
 ## gunzip -c uniref100.fasta.gz | head -15687827 > uniref.fasta
 ##
-## Serial Code (without MCE)
-##   Line driven:   15.649s       CPAN FastaReader, BioUtil-2014.1226
-##   Record driven:  8.057s       Nearly 2x faster serially
+## Running serially (one core)
+##   Perl, line driven    15.649s      FastaReader, BioUtil-2014.1226
+##   Perl, record driven   8.057s      By input record separator "\n>"
 ##
-## MCE Code -- Parallelization
-##   Line driven:    4.872s       CPAN FastaReader, BioUtil-2014.1226
-##   Record driven:  2.507s
+## Many-Core Engine
+##   Perl, line driven     4.872s      FastaReader, BioUtil-2014.1226
+##   Perl, record driven   2.507s      By input record separator "\n>"
 
 sub FastaReader {
    my ($file, $not_trim) = @_;
@@ -64,13 +66,15 @@ sub FastaReader {
    };
 }
 
+###############################################################################
+
 package main;
 
 use Cwd 'abs_path'; ## Insert lib-path at the head of @INC.
 use lib abs_path($0 =~ m{^(.*)[\\/]} && $1 || abs_path) . '/../lib';
 
+use MCE::Flow chunk_size => '1024k', max_workers => 'auto';
 use Time::HiRes qw(time);
-use MCE::Flow;
 
 ## Iterator for preserving output order.
 
@@ -92,14 +96,11 @@ sub output_iterator {
 ## Run in parallel.
 
 my $fasta_file = shift || \*DATA;
-
-my $not_trim = 0;
 my $start = time;
 
 mce_flow_f {
-   chunk_size => '512k', max_workers => 'auto',
-   RS => "\n>", use_slurpio => 1,
    gather => output_iterator,
+   RS => "\n>", use_slurpio => 1,
 },
 sub {
    my ($mce, $slurp_ref, $chunk_id) = @_;
@@ -109,22 +110,23 @@ sub {
    ${ $slurp_ref } = '>' . ${ $slurp_ref } if $chunk_id > 1;
 
    ## read from scalar reference
-   my $next_seq = BioUtil::Seq::FastaReader($slurp_ref, $not_trim);
+   my $next_seq = BioUtil::Seq::FastaReader($slurp_ref, 0);
 
+   ## loop through sequences in $slurp_ref
    while (my $fa = &$next_seq()) {
       ($hdr, $seq) = @{ $fa };
       $output .= ">$hdr\n".$seq."\n";
    }
 
-   ## send to manager process
+   ## gather output for this chunk
    MCE->gather($chunk_id, $output);
 
 }, $fasta_file;
 
 printf {*STDERR} "\n## Compute time: %0.03f\n\n", time - $start;
 
+###############################################################################
 __END__
-
 >seq1 description1
 TAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCC
 TAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCAACCCTAACCCT
