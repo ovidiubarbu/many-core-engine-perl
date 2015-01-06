d32d693dca6adf3aad38e9829f3a95443886f390
marioeroy <marioeroy@280a465b-a97f-b54b-bb6b-c9981b522c87>
2015-01-06 23:12:01 +0000

    Revert commit r708.

--- trunk/examples/include/FastaReaderFai.pm
+++ trunk/examples/include/FastaReaderFai.pm
@@ -72,7 +72,7 @@ sub Reader {
                }
             }
             $c4  =  (substr($seq, ++$c5 - 2, 1) eq "\r") ? $c5 - 2 : $c5 - 1;
-            $seq =~ s/\s//g;
+            $seq =~ tr/\t\r\n //d;
             $c2  =  length($seq);
 
             undef $seq if length($seq) > 500_000;  ## lowers memory consumption
