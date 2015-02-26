9b0758d25380eb074f8380a48382d00749ac556d
marioeroy <marioeroy@280a465b-a97f-b54b-bb6b-c9981b522c87>
2015-02-26 11:28:07 +0000

    Added examples demonstrating parallel concurrency, asynchrony, and shared data. Added step_mon.pl. Moved ping.pl under asynchrony.

---
+++ examples/asynchrony/echoserver_ae.pl
@@ -0,0 +1,87 @@
+#!/usr/bin/env perl
+
+## Asynchronous code from the web and modified for Many-Core parallelism.
+## https://blog.afoolishmanifesto.com/posts/concurrency-and-async-in-perl/
+##
+## Imagine a load-balancer configured to spread the load (round-robin)
+## to ports 9501, 9502, 9503, ..., 950N.
+
+use strict; use warnings;
+
+use Cwd 'abs_path'; ## Insert lib-path at the head of @INC.
+use lib abs_path($0 =~ m{^(.*)[\\/]} && $1 || abs_path) . '/../../lib';
+
+use MCE::Flow;
+use MCE::Shared;
+
+my $ncpu = MCE::Util::get_ncpu;
+my $next = 0; mce_share \$next; # shared counter (increments by one)
+
+my $mce_opts = {
+   user_begin => sub {
+      my ($mce) = @_;
+      ## Choose a module of your choice if CPU affinity is desired.
+      ## e.g. set_cpu_affinity( $$, ($mce->wid - 1) % $ncpu );
+   },
+   max_workers => 'auto',
+};
+
+###############################################################################
+
+use AnyEvent;
+use AnyEvent::Socket;
+use AnyEvent::Handle;
+
+use Scalar::Util 'refaddr';
+
+my $mce_task = sub {
+   my ($mce) = @_; my ($pid, $wid) = ($mce->pid, $mce->wid);
+
+   my $next_obj = tied($next); # using tied object to shared variable
+   my %handles;
+
+   my $server = tcp_server undef, 9500 + $wid, sub {
+      my ($fh, $host, $port) = @_;
+      my ($disconnect, $next_id, $hdl);
+
+      ## The ->add helper method does fetch, store, and fetch in one
+      ## trip. Thus, $mce->synchronize is not necessary.
+      $next_id = $next_obj->add(1); # same as $next_id = $next += 1
+
+      $disconnect = sub {
+         my ($hdl) = @_;
+         warn "[$pid:$next_id] client disconnected\n";
+         delete $handles{ refaddr $hdl };
+         $hdl->destroy;
+      };
+
+      $hdl = AnyEvent::Handle->new(
+         fh       => $fh,
+         on_eof   => $disconnect,
+         on_error => $disconnect,
+         on_read  => sub {
+            my ($hdl) = @_;
+            $hdl->push_write($hdl->rbuf);
+            substr($hdl->{rbuf}, 0) = '';
+         },
+      );
+
+      $handles{ refaddr $hdl } = $hdl;
+
+      $hdl->{timer} = AnyEvent->timer(
+         after    => 5,
+         interval => 5,
+         cb       => sub {
+            $hdl->push_write("[$pid:$next_id] ping!\n")
+         },
+      );
+   }, sub {
+      my ($fh, $thishost, $thisport) = @_;
+      warn "[$pid:0] listening on $thishost:$thisport\n";
+   };
+
+   AnyEvent->condvar->wait();
+};
+
+MCE::Flow->run($mce_opts, $mce_task);
+
---
+++ examples/step_mon.pl
@@ -0,0 +1,107 @@
+#!/usr/bin/perl
+###############################################################################
+##
+## Monitor script contributed by George Bouras (Greece 02/2015).
+## A demonstration for MCE->step_to(...) in MCE 1.601.
+##
+###############################################################################
+
+use strict;
+use warnings;
+
+use Cwd 'abs_path'; ## Insert lib-path at the head of @INC.
+use lib abs_path($0 =~ m{^(.*)[\\/]} && $1 || abs_path) . '/../lib';
+
+use Time::HiRes 'sleep';
+use MCE::Step;
+
+my %Monitors = (
+   mon_cpu => [ qw/ srvC1 srvC2 srvC3 srvC4 srvC5 srvC6 srvC7 / ],
+   mon_dsk => [ qw/ srvD1 srvD2 srvD3 srvD4 srvD5 srvD6 srvD7 / ],
+   mon_mem => [ qw/ srvM1 srvM2 srvM3 srvC4 srvM5 srvM6 srvM7 / ]
+);
+
+## Checking frequency (in seconds) of every monitor.
+## Using fractional seconds for this demonstration.
+
+my %Schedules = (
+   0.15 => [ qw/ mon_cpu mon_dsk / ],
+   0.35 => [ qw/ mon_mem         / ]
+);
+
+###############################################################################
+##
+## Run 4 sub-tasks simultaneously using one MCE instance
+##
+###############################################################################
+
+MCE::Step::run({
+   task_name    => [ qw/ scheduler mon_cpu mon_dsk mon_mem / ],
+   max_workers  => [ scalar keys %Schedules, 10, 10, 10 ],
+   input_data   => [ sort { $a <=> $b } keys %Schedules ],
+   chunk_size   => 1,
+
+   user_begin   => sub { my ($mce, $task_id, $task_name) = @_; },
+   user_end     => sub { my ($mce, $task_id, $task_name) = @_; },
+   user_output  => sub { print STDOUT "$_[0]\n" },
+   user_error   => sub { print STDERR "$_[0]\n" },
+
+   spawn_delay  => 0,
+   submit_delay => 0,
+   job_delay    => 0,
+
+}, \&Scheduler, \&Monitor, \&Monitor, \&Monitor);
+
+MCE::Step::finish;
+
+###############################################################################
+##
+## Sub-tasks for Many-Core Engine.
+##
+###############################################################################
+
+sub Scheduler
+{
+   my $mce_shedule = shift;
+   my $chunk_ref   = shift;
+   my $chunk_id    = shift;
+   my $worker_id   = $mce_shedule->wid;
+   my $seconds     = $chunk_ref->[0];
+   my @WorkLoad;
+   my $work_ref;
+
+   ## change to while ('FOR EVER') if desired
+   for (1 .. 10) {
+      foreach my $mon (@{ $Schedules{$seconds} }) {
+
+         MCE->print(
+            "interval $seconds sec, starting monitors : " .
+            "@{ $Schedules{$seconds} }"
+         );
+
+         foreach my $srv (@{ $Monitors{$mon} }) {
+            MCE->step_to($mon, $srv);
+         }
+
+         sleep $seconds;
+
+         ## Wait until processing has completed for submitted work
+         MCE->await($mon, 10);  ## waits until 10 or less remaining
+      }
+   }
+
+   return;
+}
+
+sub Monitor
+{
+   my $mce_monitor = shift;
+   my $server      = shift;
+   my $monitor     = $mce_monitor->task_name;
+   my $worker_id   = $mce_monitor->wid;
+
+   MCE->print("monitor=$monitor , server=$server");
+
+   return;
+}
+
