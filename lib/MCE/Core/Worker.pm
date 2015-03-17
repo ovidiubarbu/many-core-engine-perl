3cdcfcff36d80ec1ba6169587a50209482fec248
marioeroy <marioeroy@280a465b-a97f-b54b-bb6b-c9981b522c87>
2015-03-17 05:56:02 +0000

    Update trunk with fixes from 1.602.

--- lib/MCE/Core/Worker.pm
+++ lib/MCE/Core/Worker.pm
@@ -604,7 +604,7 @@ sub _worker_loop {
 sub _worker_main {
 
    my ( $self, $_wid, $_task, $_task_id, $_task_wid, $_params,
-        $_plugin_worker_init, $_has_threads, $_is_MSWin32 ) = @_;
+        $_plugin_worker_init, $_has_threads, $_is_winenv ) = @_;
 
    @_ = ();
 
@@ -619,8 +619,8 @@ sub _worker_main {
    ## Define DIE handler.
    local $SIG{__DIE__} = sub {
       if (!defined $^S || $^S) {                          ## Perl state
-         my $_lmsg = Carp::longmess();
-         if ($_lmsg =~ /^[^\n]+\n\teval /) {              ## In eval?
+         my  $_lm = Carp::longmess();                     ## Inside eval?
+         if ($_lm =~ /^[^\n]+\n\teval / || $_lm =~ /\n\teval [^\n]+\n\tTry/) {
             CORE::die(@_);
          }
       }
@@ -702,7 +702,7 @@ sub _worker_main {
    if (defined $_params) {
       sleep 0.002; _worker_do($self, $_params); undef $_params;
    } else {
-      lock $MCE::_WIN_LOCK if ($_is_MSWin32);
+      lock $MCE::_WIN_LOCK if ($_is_winenv);
    }
 
    ## Enter worker loop. Clear worker session after running.
